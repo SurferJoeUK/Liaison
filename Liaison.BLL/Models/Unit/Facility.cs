2a049e315984975cc73e0a9f4c2eabc9d96f2c74
LauraHPLaptop <surferjoeuk@hotmail.com>
2021-04-03 15:15:08 +0100

    db updates, facility

--- Liaison.BLL/Models/Unit/Facility.cs
+++ Liaison.BLL/Models/Unit/Facility.cs
@@ -44,6 +44,50 @@ namespace Liaison.BLL.Models.Unit
             {
                 Liaison.Data.Sql.GetStuff.SetUnitObject(sqlUnit.UnitId, this.GetType().ToString());
             }
+
+            var concurrents = relMain.Where(c => c.RelationshipType.RelationshipTypeId == (int)HigherHqType.Concurrent);
+            foreach (var ch in concurrents)
+            {
+                if (this.UnitId == ch.RelFromUnitId)
+                {
+                    if (this.ConcsLow == null)
+                    {
+                        this.ConcsLow = new List<string>();
+                    }
+
+                    string index = ch.RelationshipsTo.UnitIndexes.Where(i => i.DisplayOrder == 30).FirstOrDefault()?.IndexCode;
+                    if (index == null)
+                    {
+                        index = ch.RelationshipsTo.UnitIndexes.Where(i => i.DisplayOrder == 20).FirstOrDefault()?.IndexCode;
+                    }
+                    if (index == null)
+                    {
+                        index = "NO IDX: " + ch.RelationshipsTo.UnitId;
+                    }
+
+                    this.ConcsLow.Add(index.Replace("_", ""));
+                }
+
+                else
+                {
+                    if (this.ConcsHigher == null)
+                    {
+                        this.ConcsHigher = new List<string>();
+                    }
+                    string index = ch.RelationshipsFrom.UnitIndexes.Where(i => i.DisplayOrder == 30).FirstOrDefault()?.IndexCode;
+                    if (index == null)
+                    {
+                        index = ch.RelationshipsFrom.UnitIndexes.Where(i => i.DisplayOrder == 20).FirstOrDefault()?.IndexCode;
+                    }
+                    if (index == null)
+                    {
+                        index = "No IDX: " + ch.RelationshipsFrom.UnitId;
+                    }
+                    this.ConcsHigher.Add(index.Replace("_", ""));
+
+                }
+            }
+
         }
 
         public Vessel Vessel { get; set; }
